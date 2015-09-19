44909ebb7fe73398dc158b400cf4f528b6391e57
Alex Palaistras <alex@deuill.org>
2015-09-19 22:19:27 +0100

    Remove deprecated `value` bindings.

--- value.c
+++
@@ -1,5 +0,0 @@
-#include "value.h"
-
-void value_destroy(void *val) {
-	zval_dtor((zval *) val);
-}
--- value.h
+++
@@ -1,9 +0,0 @@
-#ifndef VALUE_H
-#define VALUE_H
-
-/* PHP includes */
-#include <zend.h>
-
-void value_destroy(void *val);
-
-#endif
