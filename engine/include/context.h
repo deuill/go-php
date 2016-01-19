8b2bb744dcf8b560c2b01cde4fbe66bb3ab5401a
Alex Palaistras <alex@deuill.org>
2016-01-19 22:52:29 +0000

    Bump year to 2016
    Signed-off-by: Alex Palaistras <alex@deuill.org>

--- engine/include/context.h
+++ engine/include/context.h
@@ -1,4 +1,4 @@
-// Copyright 2015 Alexander Palaistras. All rights reserved.
+// Copyright 2016 Alexander Palaistras. All rights reserved.
 // Use of this source code is governed by the MIT license that can be found in
 // the LICENSE file.
 
