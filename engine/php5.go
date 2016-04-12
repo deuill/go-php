0882169cff2bc982773234e7f85c774a8cb015df
Alex Palaistras <alex@deuill.org>
2016-04-12 21:06:54 +0100

    Add necessary include directives for PHP 5 path
    Certain distributions, such as Debian, ship include files for version 5
    of PHP in the `/usr/include/php5` directory. This commit adds directives
    necessary for building go-php against packages provided by such distros.

--- engine/php5.go
+++ engine/php5.go
@@ -2,6 +2,7 @@
 
 package engine
 
-// #cgo CFLAGS: -Iinclude/php5
+// #cgo CFLAGS: -I/usr/include/php5 -I/usr/include/php5/main -I/usr/include/php5/TSRM
+// #cgo CFLAGS: -I/usr/include/Zend -Iinclude/php5
 // #cgo LDFLAGS: -lphp5
 import "C"
