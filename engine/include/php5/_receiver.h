3d31c927e37fdb3eae5b154f1caf5eebf583aa33
Alex Palaistras <alex@deuill.org>
2016-05-05 22:21:57 +0100

    Clean up receiver pre-processor binding

--- engine/include/php5/_receiver.h
+++ engine/include/php5/_receiver.h
@@ -5,63 +5,20 @@
 #ifndef ___RECEIVER_H___
 #define ___RECEIVER_H___
 
-#define RECEIVER_GET(o, m)           receiver_get(o, m, int t, const zend_literal *k)
-#define RECEIVER_SET(o, m, v)        receiver_set(o, m, v, const zend_literal *k)
-#define RECEIVER_EXISTS(o, m, c)     receiver_exists(o, m, c, const zend_literal *k)
-#define RECEIVER_METHOD_GET(o, n, l) receiver_method_get(zval **o, char *n, int l, const zend_literal *k)
-#define RECEIVER_METHOD_CALL(m)      receiver_method_call(const char *m, INTERNAL_FUNCTION_PARAMETERS)
-#define RECEIVER_CONSTRUCTOR_GET(o)  receiver_constructor_get(zval *o)
-#define RECEIVER_FREE(o)             receiver_free(void *o)
-#define RECEIVER_INIT(c)             zend_object_value receiver_init(c)
+static zval *_receiver_get(zval *object, zval *member, int type, const zend_literal *key);
+static void _receiver_set(zval *object, zval *member, zval *value, const zend_literal *key);
+static int _receiver_exists(zval *object, zval *member, int check, const zend_literal *key);
 
-#define RECEIVER_DESTROY(n) do {                                                    \
-	zend_class_entry **c;                                                           \
-	if (zend_hash_find(CG(class_table), n, strlen(n), (void **) &c) == SUCCESS) {   \
-		destroy_zend_class(c);                                                      \
-		zend_hash_del_key_or_index(CG(class_table), n, strlen(n), 0, HASH_DEL_KEY); \
-	}                                                                               \
-} while (0)
+static int _receiver_method_call(const char *method, INTERNAL_FUNCTION_PARAMETERS);
+static zend_function *_receiver_method_get(zval **object, char *name, int len, const zend_literal *key);
+static zend_function *_receiver_constructor_get(zval *object);
 
-static inline zval *RECEIVER_RETVAL() {
-	zval *val = NULL;
-	MAKE_STD_ZVAL(val);
-	return val;
-}
+static void _receiver_free(void *object);
+static zend_object_value _receiver_init(zend_class_entry *class_type);
+static void _receiver_destroy(char *name);
 
-#define RECEIVER_THIS(o)        ((engine_receiver *) zend_object_store_get_object(o))
-#define RECEIVER_STRING_COPY(n) estrndup(n, len)
-
-#define RECEIVER_FUNC()         (zend_internal_function *) EG(current_execute_data)->function_state.function
-#define RECEIVER_FUNC_NAME(m)   (char *) (m)
-#define RECEIVER_FUNC_SET_ARGFLAGS(f)
-
-#define RECEIVER_OBJECT(o) ((zend_object *) (&(RECEIVER_THIS(o)->obj)))
-#define RECEIVER_OBJECT_CREATE(r, t) do {                              \
-	r = emalloc(sizeof(engine_receiver));                              \
-	memset(r, 0, sizeof(engine_receiver));                             \
-	zend_object_std_init(&this->obj, t);                               \
-	zend_object_value object;                                          \
-	object.handle = zend_objects_store_put(r,                          \
-			(zend_objects_store_dtor_t) zend_objects_destroy_object,   \
-			(zend_objects_free_object_storage_t) receiver_free, NULL); \
-	object.handlers = &receiver_handlers;                              \
-	return object;                                                     \
-} while (0)
-
-#define RECEIVER_OBJECT_DESTROY(r) do {                                \
-	zend_object_std_dtor(&r->obj);                                     \
-	efree(r);                                                          \
-} while (0)
-
-#define RECEIVER_HANDLERS_SET(h) do {                                  \
-	zend_object_handlers *std = zend_get_std_object_handlers();        \
-	h.get_class_name  = std->get_class_name;                           \
-	h.get_class_entry = std->get_class_entry;                          \
-} while (0)
-
-// Return class name for method receiver.
-static inline char *receiver_get_name(engine_receiver *rcvr) {
-	return (char *) rcvr->obj.ce->name;
-}
+static engine_receiver *_receiver_this(zval *object);
+static void _receiver_handlers_set(zend_object_handlers *handlers);
+char *_receiver_get_name(engine_receiver *rcvr);
 
 #endif
