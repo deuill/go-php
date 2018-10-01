f7137ab32840cc217f6b5d32f183dbc6a378b219
Timm Friebe <thekid@thekid.de>
2018-10-01 20:19:14 +0200

    Use old signature for engine_log_message w/ PHP < 7.1
    See https://github.com/deuill/go-php/pull/58#issuecomment-426009629

--- engine.c
+++ engine.c
@@ -63,7 +63,11 @@ static void engine_register_variables(zval *track_vars_array) {
 	php_import_environment_variables(track_vars_array);
 }
 
+#if PHP_VERSION_ID < 70100
+static void engine_log_message(char *str) {
+#else
 static void engine_log_message(char *str, int syslog_type_int) {
+#endif
 	engine_context *context = SG(server_context);
 
 	engineWriteLog(context, (void *) str, strlen(str));
